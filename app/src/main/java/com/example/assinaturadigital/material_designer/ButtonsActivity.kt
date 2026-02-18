651bac7b8b58c911103374cadee7fd937d36d7e3
levi-acs <levi.acsinformatica@gmail.com>
2026-02-18 13:55:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/assinaturadigital/material_designer/ButtonsActivity.kt
@@ -0,0 +1,23 @@
+package com.example.assinaturadigital.material_designer
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.assinaturadigital.material_designer.databinding.ActivityButtonsBinding
+
+class ButtonsActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityButtonsBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+    }
+
+    companion object{
+        fun createIntent(context: Context): Intent {
+            return Intent(context, ButtonsActivity::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/assinaturadigital/material_designer/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.assinaturadigital.material_designer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.assinaturadigital.material_designer.databinding.ActivityMainBinding
+import kotlin.getValue
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(binding.root)
+        insertListener()
+    }
+
+    private fun insertListener() {
+
+        binding.cvButtons.setOnClickListener {
+            startActivity(ButtonsActivity.createIntent(this))
+        }
+    }
+}
